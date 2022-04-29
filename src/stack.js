93ba3b3399eeed91c7eb8afef56184ca23e03037
Vladimir <mr.katenin@mail.ru>
2022-04-29 11:49:14 +0300

    feat: add stack alg

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    stack = [];
+    push(el) {
+        this.stack.push(el);
+    }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        if (this.stack.length > 0) return this.stack.pop();
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    peek() {
+        if (this.stack.length > 0) return this.stack[this.stack.length - 1];
+    }
 }
 
 module.exports = {
-  Stack
+    Stack
 };
